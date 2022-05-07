858aeb0f51174a623fa7c1533594637fb9cee60f
Tiago Galdino <tiagogaldino@gmail.com>
2022-05-06 23:16:37 -0300

    autenticação de USUÁRIO
    adicionar novo usuário, autenticação, encrypted password.

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}))
+app.use(express.urlencoded({ extended: false}));
+
+require('./Controllers/authController')(app);
 
 const PORT = 3000;
 app.listen(PORT, () => {
