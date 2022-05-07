e25e5bcea0dedf68e3cb4e9783983228ee56dd54
Tiago Galdino <tiagogaldino@gmail.com>
2022-05-06 22:42:47 -0300

    novas atualizações
    criação de conexão com DB (mongoose) e Schema USER.

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`App running in PORT ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send("Olá mundo!")
+})
